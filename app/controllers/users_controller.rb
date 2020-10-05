e00bfb936d7393999e3744993653e917d8166181
starocketeer <muhammadfirhad1997@gmail.com>
2020-10-05 14:09:17 +0800

    chapter_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by params[:id]
   end
-    
+
   def new 
     @user = User.new
   end
-    
+
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -17,9 +18,9 @@ class UsersController < ApplicationController
       render :new
     end
   end
-    
+
   private
-    
+
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,31 @@
 class User < ApplicationRecord
-    validates :name, presence: true, 
-      length: {maximum: Settings.validations.name.max_length}
+  validates :name, presence: true, 
+    length: {maximum: Settings.validations.name.max_length}
+
+  validates :email, presence: true, 
+    length: { maximum: Settings.validations.email.max_length},
+    format: { with: Settings.validations.email.regex },
+    uniqueness: true
   
-    validates :email, presence: true, 
-      length: { maximum: Settings.validations.email.max_length},
-      format: { with: Settings.validations.email.regex },
-      uniqueness: true
-    
-    validates :password, presence: true, 
-      length: { minimum: Settings.validations.password.min_length }
-    
-    has_secure_password 
-    
-    before_save :downcase_email
-    
-    private
-    
-    def downcase_email
-      email.downcase!
+  validates :password, presence: true, 
+    length: { minimum: Settings.validations.password.min_length }
+  
+  has_secure_password 
+  
+  before_save :downcase_email
+  
+  class << User
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST :
+                                                  Bcrypt::Engine.cost
+      Bcrypt::Password.create string, cost: cost
     end
   end
+  
+  private
+  
+  def downcase_email
+    email.downcase!
+  end
+end
   
