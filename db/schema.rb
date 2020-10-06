bc3471a83584fa17685a04df4593d7e0031893d4
starocketeer <muhammadfirhad1997@gmail.com>
2020-10-06 13:22:36 +0800

    chapter_10

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-  ActiveRecord::Schema.define(version: 2020_08_28_104245) do
+ActiveRecord::Schema.define(version: 2020_09_07_101501) do
 
-    create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-      t.string "name"
-      t.string "email"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.string "password_digest"
-      t.index ["email"], name: "index_users_on_email", unique: true
-    end
-  
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+end
   
